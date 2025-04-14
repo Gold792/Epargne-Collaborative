6c1ee7267ea93e31375149216fde2884c5cf0d5e
Manzidath <manzidatholoukossi@gmail.com>
2025-04-14 15:34:05 +0100

    correction

---
+++ migrations/Version20250414140549.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250414140549 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            CREATE TABLE cotisation (id INT AUTO_INCREMENT NOT NULL, titre VARCHAR(255) NOT NULL, type_cotisation VARCHAR(20) NOT NULL, description LONGTEXT DEFAULT NULL, montant_objectif DOUBLE PRECISION NOT NULL, montant_minimum DOUBLE PRECISION NOT NULL, montant_par_echeance DOUBLE PRECISION DEFAULT NULL, date_debut DATETIME NOT NULL, date_fin DATETIME DEFAULT NULL, periodicite VARCHAR(20) DEFAULT NULL, frequence_periode INT DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE utilisateur CHANGE roles roles JSON NOT NULL
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE messenger_messages CHANGE delivered_at delivered_at DATETIME DEFAULT NULL COMMENT '(DC2Type:datetime_immutable)'
+        SQL);
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            DROP TABLE cotisation
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE messenger_messages CHANGE delivered_at delivered_at DATETIME DEFAULT 'NULL' COMMENT '(DC2Type:datetime_immutable)'
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE utilisateur CHANGE roles roles LONGTEXT NOT NULL COLLATE `utf8mb4_bin`
+        SQL);
+    }
+}
